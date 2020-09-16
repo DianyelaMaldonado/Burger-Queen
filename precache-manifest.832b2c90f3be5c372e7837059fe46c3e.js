22338bfd4674a93c5d2b49a27448b83e3d364edb
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-09-16 12:30:08 -0500

    Updates

---
+++ precache-manifest.832b2c90f3be5c372e7837059fe46c3e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8031a47d163ab0c1d2e3189b542a8b9c",
+    "url": "/Burger-Queen/index.html"
+  },
+  {
+    "revision": "4f7d7f6b931e6b26ddf7",
+    "url": "/Burger-Queen/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "41ab2456303815133474",
+    "url": "/Burger-Queen/static/css/main.e8f94d41.chunk.css"
+  },
+  {
+    "revision": "4f7d7f6b931e6b26ddf7",
+    "url": "/Burger-Queen/static/js/2.34b9a76f.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/Burger-Queen/static/js/2.34b9a76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ab2456303815133474",
+    "url": "/Burger-Queen/static/js/main.1e540a88.chunk.js"
+  },
+  {
+    "revision": "90e005892e8b7bd4da82",
+    "url": "/Burger-Queen/static/js/runtime-main.75dfe579.js"
+  },
+  {
+    "revision": "b084f4cf3ea60868bbd4c4ef17fc3943",
+    "url": "/Burger-Queen/static/media/BgLogin.b084f4cf.png"
+  },
+  {
+    "revision": "0268b4ca008f71765c3c2f47e9c9529f",
+    "url": "/Burger-Queen/static/media/Logo_BQ.0268b4ca.png"
+  },
+  {
+    "revision": "0f0c7370000ee81a16f427434b75fedc",
+    "url": "/Burger-Queen/static/media/Trastes.0f0c7370.jpg"
+  },
+  {
+    "revision": "40a3a3da4f63c80777d6e41a81414bfd",
+    "url": "/Burger-Queen/static/media/burger-lunch.40a3a3da.jpg"
+  },
+  {
+    "revision": "c4b1a556ac390e256b7cc4afab2f11fb",
+    "url": "/Burger-Queen/static/media/chess.c4b1a556.jpg"
+  },
+  {
+    "revision": "7e790d207a983b6f79132981c9cc5d5c",
+    "url": "/Burger-Queen/static/media/coffe-break.7e790d20.png"
+  },
+  {
+    "revision": "a5b4a2c3a8adc99d40973be4712f099a",
+    "url": "/Burger-Queen/static/media/cola.a5b4a2c3.jpg"
+  },
+  {
+    "revision": "9f3d0fd90f0daeb9b0550d60e17460a6",
+    "url": "/Burger-Queen/static/media/cuadrado-bordes.9f3d0fd9.png"
+  },
+  {
+    "revision": "f2c4647d858b0976157a2be2fc7c11c5",
+    "url": "/Burger-Queen/static/media/doble-burger.f2c4647d.jpg"
+  },
+  {
+    "revision": "5eaa6e4c5e66f876472f7e8e29518e50",
+    "url": "/Burger-Queen/static/media/eggs.5eaa6e4c.jpg"
+  },
+  {
+    "revision": "a897d0464e957a72f9a4557cdd0fc76e",
+    "url": "/Burger-Queen/static/media/fondo-sarten.a897d046.png"
+  },
+  {
+    "revision": "1fe2ad019f0b381a5036121f64eee1b3",
+    "url": "/Burger-Queen/static/media/fries.1fe2ad01.jpg"
+  },
+  {
+    "revision": "dae19e728533e918c674f2a77c402e73",
+    "url": "/Burger-Queen/static/media/hat-chef.dae19e72.png"
+  },
+  {
+    "revision": "68f8eef731fba05febedd3ee098068b8",
+    "url": "/Burger-Queen/static/media/ketchup.68f8eef7.png"
+  },
+  {
+    "revision": "f896df0c28013992d6ed3dde734b4415",
+    "url": "/Burger-Queen/static/media/kitchen-hat-chef.f896df0c.png"
+  },
+  {
+    "revision": "86affa9db01ea9c8fdf2a11b52fe1c6c",
+    "url": "/Burger-Queen/static/media/onion-rings.86affa9d.jpg"
+  },
+  {
+    "revision": "85efadd3b322df2d2ef1f7fe8ae701c4",
+    "url": "/Burger-Queen/static/media/pink-circle.85efadd3.png"
+  },
+  {
+    "revision": "4c8a0914a79ddb8db63e39bf62bd45d1",
+    "url": "/Burger-Queen/static/media/salsas.4c8a0914.png"
+  },
+  {
+    "revision": "8ac4d3d56eaa0a801c3438c9aca08e66",
+    "url": "/Burger-Queen/static/media/simple-burger.8ac4d3d5.jpg"
+  },
+  {
+    "revision": "6d902638d221637aede35d40e8c8d319",
+    "url": "/Burger-Queen/static/media/waiter.6d902638.png"
+  },
+  {
+    "revision": "668c04aedcdd41a0dddc535627cf2049",
+    "url": "/Burger-Queen/static/media/water-bottle.668c04ae.jpeg"
+  }
+]);
